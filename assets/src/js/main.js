c1fe96f83468e447e48f03e7adaad63fc362f45e
Javier Benavides <chato.javier@gmail.com>
2022-02-15 11:43:00 -0500

    fixing gallery for safari browser

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,40 +1,40 @@
-import "../sass/main.scss";
-import "./components/menu";
+import '../sass/main.scss';
+import './components/menu';
 
 // import Swiper JS
-import Swiper from "swiper";
+import Swiper from 'swiper';
 
 // core version + navigation, pagination modules:
-import SwiperCore, { Navigation, Thumbs, Lazy, Controller } from "swiper/core";
+import SwiperCore, { Navigation, Thumbs, Lazy, Controller } from 'swiper/core';
 
 // configure Swiper to use modules
 SwiperCore.use([Navigation, Lazy, Controller]);
 
 // Loop Carousel
-if (document.querySelector(".loop__content--carousel")) {
-	const slidersCount = document.querySelectorAll(".loop_slider").length;
+if (document.querySelector('.loop__content--carousel')) {
+	const slidersCount = document.querySelectorAll('.loop_slider').length;
 	const windowWidth = window.innerWidth;
 	if (
 		(windowWidth >= 640 && slidersCount < 3) ||
 		(windowWidth >= 768 && slidersCount < 4)
 	) {
-		const swiperContainer = document.querySelector(".swiper-container");
-		const swiperWrapper = document.querySelector(".swiper-wrapper");
-		const buttonprev = document.querySelector(".slider-button-prev");
-		const buttonnext = document.querySelector(".slider-button-next");
-		swiperContainer.classList.remove("loop__content--carousel");
-		swiperWrapper.classList.add("justify-center");
-		swiperWrapper.classList.add("space-x-5");
-		buttonprev.classList.add("hidden");
-		buttonnext.classList.add("hidden");
+		const swiperContainer = document.querySelector('.swiper-container');
+		const swiperWrapper = document.querySelector('.swiper-wrapper');
+		const buttonprev = document.querySelector('.slider-button-prev');
+		const buttonnext = document.querySelector('.slider-button-next');
+		swiperContainer.classList.remove('loop__content--carousel');
+		swiperWrapper.classList.add('justify-center');
+		swiperWrapper.classList.add('space-x-5');
+		buttonprev.classList.add('hidden');
+		buttonnext.classList.add('hidden');
 	}
-	window.addEventListener("load", () => {
-		let swiper = new Swiper(".loop__content--carousel", {
-			slidesPerView: "auto",
+	window.addEventListener('load', () => {
+		let swiper = new Swiper('.loop__content--carousel', {
+			slidesPerView: 'auto',
 			spaceBetween: 20,
 			navigation: {
-				nextEl: ".slider-button-next",
-				prevEl: ".slider-button-prev",
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev',
 			},
 			centeredSlides: true,
 			// Responsive breakpoints
@@ -50,65 +50,126 @@ if (document.querySelector(".loop__content--carousel")) {
 
 // Gallery Carousel
 
-if (document.querySelector(".gallery__content--carousel")) {
-	const windowWidth = window.innerWidth;
-	const gallerySliders = document.querySelectorAll(".gallery__slider img");
-	if (windowWidth >= 768 && windowWidth < 1024) {
-		gallerySliders.forEach((img) => {
-			if (img.offsetWidth >= 768) {
-				console.log(img.offsetWidth);
-				console.log(img.offsetHeight);
-				const newWidth = windowWidth;
-				const newHeight =
-					(img.offsetHeight * newWidth) / img.offsetWidth;
-				img.style.width = newWidth + "px";
-				img.style.height = newHeight + "px";
-			}
-		});
-	}
+//When all document is load.
+window.addEventListener('load', () => {
+	// if gallery__content--carousel class exist
+	if (document.querySelector('.gallery__content--carousel')) {
+		const windowWidth = window.innerWidth;
+		const gallerySlider = document.querySelector(
+			'.gallery__content--carousel'
+		);
+		const gallerySlides = gallerySlider.querySelectorAll('.gallery__slide');
 
-	//When all document is load.
-	window.addEventListener("load", () => {
-		// if gallery__content--carousel class exist
-		if (document.querySelector(".gallery__content--carousel")) {
-			// create swiper slider
-			let swiper2 = new Swiper(".gallery__content--carousel", {
-				slidesPerView: 1,
-				centeredSlides: true,
-				spaceBetween: 12,
-				navigation: {
-					nextEl: ".slider-button-next",
-					prevEl: ".slider-button-prev",
-				},
-				lazy: {
-					loadPrevNext: true,
-				},
-				keyboard: {
-					enabled: true,
-					onlyInViewport: false,
-				},
-				// Responsive breakpoints
-				breakpoints: {
-					// when window width is >= 768px
-					768: {
-						slidesPerView: "auto",
-						lazy: {
-							loadPrevNextAmount: 4,
-						},
-					},
-				},
+		// fix safary desktop gallery
+		if (windowWidth >= 768) {
+			gallerySlides.forEach((slide) => {
+				if (slide.dataset.width >= windowWidth) {
+					const newWidth = windowWidth;
+					const newHeight =
+						(slide.dataset.height * newWidth) / slide.dataset.width;
+					slide.dataset.width = newWidth;
+					slide.dataset.height = newHeight;
+				}
+
+				const slideWidth = slide.dataset.width * 0.95;
+				const slideHeight = slide.dataset.height * 0.95;
+				slide.style.width = slideWidth + 'px';
+				slide.style.height = slideHeight + 'px';
 			});
 		}
-	});
-}
+
+		// create swiper slider
+		let swiper2 = new Swiper('.gallery__content--carousel', {
+			slidesPerView: 1,
+			centeredSlides: true,
+			spaceBetween: 12,
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev',
+			},
+			lazy: {
+				loadPrevNext: true,
+				loadPrevNextAmount: 2,
+			},
+			speed: 500,
+			keyboard: {
+				enabled: true,
+				onlyInViewport: false,
+			},
+			// Responsive breakpoints
+			breakpoints: {
+				// when window width is >= 768px
+				768: {
+					slidesPerView: 'auto',
+					lazy: {
+						loadPrevNextAmount: 4,
+					},
+				},
+			},
+			on: {
+				init: function () {
+					if (windowWidth >= 768) {
+						const slideActive = gallerySlider.querySelector(
+							'.swiper-slide-active'
+						);
+						const activeWidth = slideActive.dataset.width;
+						const activeHeight = slideActive.dataset.height;
+
+						slideActive.style.width = activeWidth + 'px';
+						slideActive.style.height = activeHeight + 'px';
+					}
+				},
+				slideChangeTransitionStart: function () {
+					if (windowWidth >= 768) {
+						const slideActive = gallerySlider.querySelector(
+							'.swiper-slide-active'
+						);
+						const firstSlide = gallerySlider.querySelector(
+							'.swiper-wrapper > div:first-of-type'
+						);
+						const slidePrev =
+							gallerySlider.querySelector('.swiper-slide-prev');
+						const slideNext =
+							gallerySlider.querySelector('.swiper-slide-next');
+						const activeWidth = slideActive.dataset.width;
+						const activeHeight = slideActive.dataset.height;
+
+						slideActive.style.width = activeWidth + 'px';
+						slideActive.style.height = activeHeight + 'px';
+
+						if (slidePrev) {
+							const prevWidth = slidePrev.dataset.width * 0.95;
+							const prevHeight = slidePrev.dataset.height * 0.95;
+							slidePrev.style.width = prevWidth + 'px';
+							slidePrev.style.height = prevHeight + 'px';
+						}
+
+						if (slideNext) {
+							const nextWidth = slideNext.dataset.width * 0.95;
+							const nextHeight = slideNext.dataset.height * 0.95;
+							slideNext.style.width = nextWidth + 'px';
+							slideNext.style.height = nextHeight + 'px';
+						}
+
+						if (slideActive == firstSlide) {
+							firstSlide.style.marginLeft = 0;
+						} else {
+							firstSlide.style.marginLeft = '-20px';
+						}
+					}
+				},
+			},
+		});
+	}
+});
 
 //Make allways visible the "hover" in archive Courses and Productions in mobile devises
 if (
-	document.querySelector(".post-type-archive-courses") ||
-	document.querySelector(".post-type-archive-productions")
+	document.querySelector('.post-type-archive-courses') ||
+	document.querySelector('.post-type-archive-productions')
 ) {
 	if (window.innerWidth <= 1024) {
-		const hoverThumbs = document.querySelectorAll(".loop__hover_info");
+		const hoverThumbs = document.querySelectorAll('.loop__hover_info');
 		hoverThumbs.forEach((thumb) => {
 			thumb.style.opacity = 1;
 		});
@@ -117,12 +178,12 @@ if (
 
 // show main content when page is loaded.
 const showContent = () => {
-	const content = document.querySelector(".site-main");
-	const spin = document.querySelector(".loading-spin");
+	const content = document.querySelector('.site-main');
+	const spin = document.querySelector('.loading-spin');
 	content.style.opacity = 1;
-	spin ? (spin.style.opacity = 0) : "";
+	spin ? (spin.style.opacity = 0) : '';
 };
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
 	showContent();
 });
--- template-parts/gallery.php
+++ template-parts/gallery.php
@@ -13,20 +13,20 @@ if ($gallery) :
 <!-- Swiper -->
 <div class="gallery__content relative">
     <div class="swiper-container gallery__content--carousel">
-        <div class="swiper-wrapper h-90 md:h-96">
+        <div class="swiper-wrapper h-90 md:h-96 items-center">
             <?php
             foreach( $gallery as $image_id ):
-                $image1x      = wp_get_attachment_image_url( $image_id, 'aa_gallery' );
-                $image2x      = wp_get_attachment_image_url( $image_id, 'aa_gallery_retina' );
-                $image_alt    = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
                 $image_data   = wp_get_attachment_image_src( $image_id, 'aa_gallery' );
+                $image_src    = $image_data[0];
+                $image_srcset = wp_get_attachment_image_srcset( $image_id, 'aa_gallery_retina' );
+                $image_alt    = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
                 $image_height = $image_data[2];
                 $image_width  = $image_data[1];
                 $img_height   = 384;
                 $img_width    = intval(($img_height * $image_width) / $image_height)
                 ?>
-                <div class="gallery__slider | swiper-slide flex items-center justify-center h-auto w-auto bg-no-repeat bg-center" style="background-image: url('<?php echo esc_url(AA_BUILD_IMG_URI . '/Spin-1s-50px.svg') ?>'); background-size: 25px 25px;">
-                    <img data-src="<?php echo $image1x; ?>" alt="<?php echo $image_alt; ?>" data-srcset="<?php echo $image2x; ?> 2x" class="gallery__img | h-auto w-auto max-h-90 md:max-h-none md:h-full swiper-lazy" width="<?php echo $img_width; ?>" height="<?php echo $img_height; ?>">
+                <div class="gallery__slide | swiper-slide bg-no-repeat bg-center max-h-full md:h-full w-auto flex md:block items-center" style="background-image: url('<?php echo esc_url(AA_BUILD_IMG_URI . '/Spin-1s-50px.svg') ?>'); background-size: 25px 25px;" data-width="<?php echo $img_width; ?>" data-height="<?php echo $img_height; ?>">
+                    <img data-src="<?php echo $image_src; ?>" alt="<?php echo $image_alt; ?>" data-srcset="<?php echo $image_srcset; ?>" sizes="(min-width: 768px) <?php echo $img_width; ?>px, 100vw" class="gallery__img swiper-lazy md:h-full w-auto max-h-full mx-auto">
                 </div>
                 <?php
             endforeach;
